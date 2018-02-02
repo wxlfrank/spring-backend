675e44543c827c2fb6ca8326755813c390030772
wxlfrank <wxlfrank@gmail.com>
2018-02-02 14:15:42 +0100

    init spring backend project in gradle

---
+++ src/main/java/org/example/demo/be/BackendDemoApplication.java
@@ -0,0 +1,12 @@
+package org.example.demo.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/example/demo/be/BackendDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.example.demo.be;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
